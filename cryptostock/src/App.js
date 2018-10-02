2d1b9198ba0d535be56521b0811265b378bb4f6a
Vladislav_747 <zippo585@gmail.com>
2018-10-03 02:06:05 +0500

    Keep Working

---
+++ cryptostock/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+//Array
+//var currencies = ["hui"] ;
+import currencies from './cryptocurrencies'
+import Ticker from './Ticker/Ticker'
+class App extends Component {
+
+  //Ставим изначальный state
+  state = {
+    selectedPairs: [],
+  };
+
+  handleCheckbox = currency => (event) => {
+
+
+
+    //равносильно const checked = event.target.checked;
+    const { checked } = event.target;
+
+
+
+
+    this.setState(({ selectedPairs }) => {
+
+      //спреадоператор Все что было в массиве положил в новый массив
+      let pairs = [...selectedPairs];
+
+      if (checked) {
+        pairs.push(currency);
+        console.log(pairs);
+      } else {
+        pairs = pairs.filter(pair => pair !== currency);
+      }
+
+      return {
+        selectedPairs: pairs,
+      }
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Крипто биржа</h1>
+        </header>
+        <aside>
+          <ul className="currList">
+            {/*Для каждого элемента массива currencies мы создаем <li>   */}
+            {currencies.map(curr => (
+              <li key={curr} className="currItem">
+                <input type="checkbox" id={curr} onChange={this.handleCheckbox(curr)} />
+                <label htmlFor={curr}>{curr.toUpperCase()}</label>
+              </li>
+
+            ))}
+          </ul>
+        </aside>
+
+        <main>
+          {this.state.selectedPairs.map(pair => <Ticker key={pair} pair={pair} />)}
+
+
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ cryptostock/src/cryptocurrencies.js
@@ -0,0 +1,15 @@
+
+var currencies = 
+[   "42 Coin",
+   "300 token",
+    "365Coin",
+    "404Coin",
+    "SixEleven",
+    "808",
+     "BTC Lite",
+    "BitConnect Coin",
+   "Bitcoin Gold",
+];
+
+
+module.exports = currencies;
