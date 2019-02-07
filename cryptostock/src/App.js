72d4eaa8b1588b1bdce71efe0f0cf1f37c497920
Vladislav_747 <zippo585@gmail.com>
2019-02-07 22:12:57 +0500

    Added semantic-ui, few css fixes

--- cryptostock/src/App.js
+++ cryptostock/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import ReactDOM from 'react-dom';
 import './App.css';
-import $ from 'jquery'; 
-//Array
+import $ from 'jquery';
+import { Checkbox, Segment } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
+//Staic Array
 import currencies from './cryptocurrencies'
 import Ticker from './Ticker/Ticker'
 class App extends Component {
@@ -17,30 +19,20 @@ class App extends Component {
 
   //Сработка на клик
   handleCheckbox = currency => (event) => {
-
-
-
     //равносильно const checked = event.target.checked;
     const { checked } = event.target;
-
-
-
-
     this.setState(({ selectedPairs, activePairs }) => {
-
       //спреадоператор Все что было в массиве положил в новый массив
       let pairs = [...selectedPairs];
-
       if (checked) {
         pairs.push(currency);
         console.log(pairs);
       } else {
         //Если отсутсвует галочка(checked ) то убираем этот элемент из массива
-       //currency значение элемента валюта - сравниваем с каждым элементом массива 
-       //чтобы не один не равнялся ему и тогда убираем
+        //currency значение элемента валюта - сравниваем с каждым элементом массива 
+        //чтобы не один не равнялся ему и тогда убираем
         pairs = pairs.filter(pair => pair !== currency);
       }
-
       //Возвращаем state который положили
       return {
         selectedPairs: pairs,
@@ -49,47 +41,36 @@ class App extends Component {
   };
 
 
-//В далнейшем для интерактивного вывода с API
+  //В далнейшем для интерактивного вывода с API
   getCurrenciesApi() {
-    
     let responseMap;
     return fetch('https://api.coinmarketcap.com/v2/ticker/?limit=10')
       .then((response) => response.json())
-      .then((responseJson) => { 
+      .then((responseJson) => {
         responseMap = responseJson.data;
-          console.log('getCurrenciesApi ' + responseMap)  
-            
+        console.log('getCurrenciesApi ' + responseMap)
         return responseMap;
-       })
-       .then(res => {
-        
+      })
+      .then(res => {
         this.setState({
-        currenciesList: res,
-        });   
+          currenciesList: res,
+        });
         console.log(this.state.currenciesList)
-       })
-
-      
+      })
       .catch((error) => {
         console.error(error);
       })
-    
-  
-}  
-    
+  }
 
-componentDidMount(){
-  //this.getCurrenciesApi();
-  let currenciesList = this.state.currenciesList;
-}
-
-
-    
+  componentDidMount() {
+    //this.getCurrenciesApi();
+    let currenciesList = this.state.currenciesList;
+  }
 
   render() {
- if(this.state.currenciesList){
-let currenciesList = this.state.currenciesList;
-}
+    if (this.state.currenciesList) {
+      let currenciesList = this.state.currenciesList;
+    }
 
 
     return (
@@ -103,25 +84,25 @@ let currenciesList = this.state.currenciesList;
             {/*Для каждого элемента массива currencies мы создаем <li>   */}
             {currencies.map(curr => (
               <li key={curr} className="currItem">
-                <input type="checkbox" id={curr} onChange={this.handleCheckbox(curr)} />
-                <label htmlFor={curr}>{curr.toUpperCase()}</label>
+                <Segment compact>
+                  <Checkbox toggle id={curr} onChange={this.handleCheckbox(curr)}></Checkbox>  <label className="currenciesLabel" htmlFor={curr}>{curr.toUpperCase()}</label>
+                </Segment>
               </li>
-
             ))}
-
-
-
-
           </ul>
         </aside>
 
         <main>
-        {/* Здесь будет выводиться список элементов которые были КЛИКнуты*/}
-          {this.state.selectedPairs.map(pair => <Ticker key={pair} pair={pair} isActive={this.state.activePairs} />)}
-
-
+          <Card.Group>
+            {/* Здесь будет выводиться список элементов которые были Кликнуты*/}
+            {this.state.selectedPairs.map(pair =>
+              <Card>
+                <Ticker className="currenciesTicker" key={pair} pair={pair} isActive={this.state.activePairs} />
+              </Card>
+            )}
+          </Card.Group>
         </main>
-       
+
       </div>
     );
   }
--- cryptostock/src/Ticker/Ticker.js
+++ cryptostock/src/Ticker/Ticker.js
@@ -4,124 +4,86 @@ import './Ticker.css';
 
 export default class Ticker extends React.Component {
 
-   state = {
-       value: 0,
-   };
+  state = {
+    value: 0,
+  };
 
-   /**
-    *  //Функция для Работы с JSON массивами
-    */
-   fetchData = () => {
-   
-    
+  /**
+   *  //Функция для Работы с JSON массивами
+   */
+  fetchData = () => {
     return fetch('https://s2.coinmarketcap.com/generated/search/quick_search_exchanges.json')
-   
-    .then(response => response.json())
-    .then((response) => {
+      .then(response => response.json())
+      .then((response) => {
         var res1 = response;
-           console.log(res1[0].name);  
-    })
-    .then((responseJson) => {
-        console.log(responseJson);  
+        console.log(res1[0].name);
+      })
+      .then((responseJson) => {
+        console.log(responseJson);
         return responseJson;
-     this.setState({
-    value: responseJson.last,
-    });
-    })
-    .catch((error) => {
+        this.setState({
+          value: responseJson.last,
+        });
+      })
+      .catch((error) => {
         console.error(error);
       });
+  }
 
-    
-   
-    
-    //  .catch((error) {
-    //     console.error(error);
-    //   });
-   }
-
-/**
-    *  //Функция для Работы с JSON объектами
-    */
-   getMoviesFromApiAsync() {
+  /**
+      *  //Функция для Работы с JSON объектами
+      */
+     getMoviesFromApiAsyncFromFacebook() {
     return fetch('https://facebook.github.io/react-native/movies.json')
       .then((response) => response.json())
-      .then((responseJson) => {          
+      .then((responseJson) => {
         return responseJson.movies[0].releaseYear;
-       })
-       .then(res => {
+      })
+      .then(res => {
         this.setState({
-        value: res,
-        });   
-       })
-       
-      
+          value: res,
+        });
+      })
       .catch((error) => {
         console.error(error);
       });
-
-     
   }
 
-  getMoviesFromApiAsync1() { 
+  getMoviesFromApiAsync() {
     return fetch('https://api.coinmarketcap.com/v2/ticker/1/')
       .then((response) => response.json())
-      .then((responseJson) => { 
-          console.log(responseJson.data.quotes.USD.price);         
+      .then((responseJson) => {
+        console.log(responseJson.data.quotes.USD.price);
         return responseJson.data.quotes.USD.price;
-       })
-       .then(res => {
+      })
+      .then(res => {
         this.setState({
-        value: res,
-        });   
-       })
-      
+          value: res,
+        });
+      })
       .catch((error) => {
         console.error(error);
       });
+  }
 
-     
+  constructor(props) {
+    //Если вдруг используем наследование то props не потеряетс
+    super(props);
+    console.log(props);
+    this.getMoviesFromApiAsync();
+   }
+  render() {
+    const { pair } = this.props;
+    return (<div className="ticker">
+      <p>{pair.toLowerCase()}</p>
+      <p>{this.state.value} USD</p>
+    </div>
+    )
   }
   
-    constructor (props){
-        //Если вдруг используем наследование то props не потеряетс
-      super(props);
-      console.log(props);
-    //   this.getMoviesFromApiAsync();
-      this.getMoviesFromApiAsync1();
-      //setInterval(this.getMoviesFromApiAsync);
-
-      
-    }
-
-
-render(){
-    const {pair} = this.props; 
-    return ( <div className="ticker">
-    <p>{pair.toLowerCase()}</p>
-    <p>{this.state.value} USD</p>
-</div>
-)}
-
-//Элемент присоединился к Virtual Dom
-componentDidMount(){
-//this.fetchData();
-//this.interval = setInterval(this.fetchData, 10000);
-this.getMoviesFromApiAsync();
-}
- //вызывается непосредственно перед рендерингом компонента
-    componentWillUnmount() {
-    //    clearInterval(this.interval);
-    }
+  //Элемент присоединился к Virtual Dom
+  componentDidMount() {
+    this.getMoviesFromApiAsyncFromFacebook();
+  }
 }
 
-//Раньше экспортировали через функцию
-
-// export default function Ticker({pair}){
-//     return (
-//         <div className="ticker">
-//             <p>{pair.toLowerCase()}</p>
-//             <p>12000</p>
-//         </div>
-//     )
-// }
