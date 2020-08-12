b6cb0b1f631a9cd2da8ff43ef43ff114f4a595d7
Vladislav_747 <zippo585@gmail.com>
2020-08-12 12:12:00 +0500

    Bug Fixes

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -30,7 +30,7 @@ export default class Ticker extends React.Component {
       });
   }
 
-  getMoviesFromApiAsync(currency) {
+  getCurrency(currency) {
     return fetch('https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=' + currency + '&convert=USD', {
       headers: {
         'X-CMC_PRO_API_KEY': '523dd32d-0443-4acb-bad3-00dbab6f344d',
@@ -100,13 +100,13 @@ export default class Ticker extends React.Component {
   }
 
   constructor(props) {
-    //Если вдруг используем наследование то props не потеряетс
+    //Если вдруг используем наследование то props не потеряется
     super(props);
     console.log(props);
 
-    this.getMoviesFromApiAsync("BTC");
+    this.getCurrency("BTC");
     var currency = this.getApiAbbreviationFromCurrency(props.pair);
-    this.getMoviesFromApiAsync(currency);
+    this.getCurrency(currency);
   }
   render() {
     const { pair } = this.props;
