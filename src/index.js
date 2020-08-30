f671fab492298c12220fc6ea39500d4623fbcb2c
Vladislav_747 <zippo585@gmail.com>
2020-08-30 16:17:14 +0500

    Refactored path - added ~ alias, made component system more comfortable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './components/App';
+import ReactDOM from "react-dom";
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App.js";
 
+ReactDOM.render(<App />, document.getElementById("root"));
